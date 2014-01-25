67f36dba0d03dbcd9c7b66126c62c78154053d91
Bowie Xu <fantast.xu@gmail.com>
2014-01-25 23:04:29 +0800

    灵体的自由飞行模式基本ok(可以用Joystick和Oculus)来控制

--- Unity3dProject/Assets/OVR/Editor/OVRPlayerControllerEditor.cs
+++ Unity3dProject/Assets/OVR/Editor/OVRPlayerControllerEditor.cs
@@ -54,7 +54,8 @@ public class OVRPlayerControllerEditor : Editor
 			m_Component.BackAndSideDampen = EditorGUILayout.Slider("Back and Side Dampen", 	m_Component.BackAndSideDampen,0, 1);
 //			m_Component.JumpForce 		  = EditorGUILayout.Slider("Jump Force", 			m_Component.JumpForce, 		  0, 10);
 			m_Component.RotationAmount 	  = EditorGUILayout.Slider("Rotation Amount", 		m_Component.RotationAmount,   0, 5);
-				
+			m_Component.cameraTrans		  = (Transform)EditorGUILayout.ObjectField(m_Component.cameraTrans, typeof(Transform), true);	
+
 			OVREditorGUIUtility.Separator();
 
 			m_Component.GravityModifier = EditorGUILayout.Slider("Gravity Modifier", m_Component.GravityModifier, 0, 1);
