d9ec2fb54acf71278bc761bdc119d1010faed8d4
yibojiang <yibo.jiang@coconutislandstudio.com>
2014-01-26 03:16:07 +0800

    todo add password for ghost in the first room

--- Unity3dProject/Assets/Script/PianoButton.cs
+++ Unity3dProject/Assets/Script/PianoButton.cs
@@ -6,6 +6,8 @@ public class PianoButton : InteractiveObj {
 	private Tweener t;
 
 	public AudioClip sound;
+
+	public GameObject piano;
 	// Use this for initialization
 	void Start () {
 	
@@ -17,11 +19,19 @@ public class PianoButton : InteractiveObj {
 	}
 	
 	override public void Action(GameObject _actionBody){
+		if (sound!=null){
+			piano.audio.PlayOneShot(sound);	
+		}
+		
+		/*
 		if (t!=null){
 			HOTween.Restart(t);
+			t=null;
+			return;
 		}
+		*/
 		t=HOTween.To(transform, 0.2f, new TweenParms()
-            .Prop("position", new Vector3(0, -0.03f, 0), true) // Position tween (set as relative)
+            .Prop("localPosition", new Vector3(0, -0.03f, 0), false) // Position tween (set as relative)
             //.Prop("rotation", new Vector3(0, 1024, 0), true) // Relative rotation tween (this way rotations higher than 360 can be applied)
             .Loops(2, LoopType.Yoyo) // Infinite yoyo loops
             .Ease(EaseType.EaseInOutQuad) // Ease
