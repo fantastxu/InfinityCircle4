d36c69e67f6b0dae9369614e2da7895549dc5b82
Ye Feng <ye.feng@coconutislandstudio.com>
2014-01-26 10:27:49 +0800

    Update 2 door prefabs and TV tuner

--- Unity3dProject/Assets/Script/SimpleDrawer.cs
+++ Unity3dProject/Assets/Script/SimpleDrawer.cs
@@ -7,6 +7,8 @@ public class SimpleDrawer : InteractiveObj {
     public bool isOpened = false;
     public float drawerLength = 0.15f;
     
+    public AudioClip[] drawAudio = new AudioClip[2];
+    
 	// Use this for initialization
 	void Start () {
 	}
@@ -25,11 +27,23 @@ public class SimpleDrawer : InteractiveObj {
         {
             HOTween.To(animObj.transform, 1, "localPosition", new Vector3(pos.x, pos.y, pos.z - drawerLength));
             isOpened = false;
+            
+            AudioSource audio = GetComponent<AudioSource>();
+            if (audio != null && drawAudio[1] != null)
+            {
+                audio.PlayOneShot(drawAudio[1]);
+            }
         }
         else
         {
             HOTween.To(animObj.transform, 1, "localPosition", new Vector3(pos.x, pos.y, pos.z + drawerLength));
             isOpened = true;
+            
+            AudioSource audio = GetComponent<AudioSource>();
+            if (audio != null && drawAudio[0] != null)
+            {
+                audio.PlayOneShot(drawAudio[0]);
+            }
         }
 	}
 }
