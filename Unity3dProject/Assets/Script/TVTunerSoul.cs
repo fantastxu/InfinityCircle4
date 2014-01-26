2b262d8fe6be9290ce631072be781d1b771e6676
Ye Feng <ye.feng@coconutislandstudio.com>
2014-01-26 12:19:17 +0800

    TV Tuner now has attachable ability

---
+++ Unity3dProject/Assets/Script/TVTunerSoul.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TVTunerSoul : AttachableObj {
+
+    Material savedMaterial;
+    public Material secretMaterial;
+    
+    public MeshRenderer screenMesh;
+    
+	// Use this for initialization
+	void Start () {
+	}
+	
+	//triggered when soul attach itself to an attachable obj
+	public override void Active()
+	{
+        if (screenMesh != null && secretMaterial != null)
+        {
+            savedMaterial = screenMesh.materials[0];
+            
+            Material[] mats = screenMesh.materials;
+            mats[0] = secretMaterial;
+            
+            screenMesh.materials = mats;
+        }
+        
+        AudioSource audio = GetComponent<AudioSource>();
+        if (audio != null)
+        {
+            audio.Play();
+        }
+	}
+
+	//triggered when soul detach from obj 
+	public override void Deactive()
+	{
+        if (screenMesh != null && savedMaterial != null)
+        {
+            Material[] mats = screenMesh.materials;
+            mats[0] = savedMaterial;
+            
+            screenMesh.materials = mats;
+        }
+        
+        AudioSource audio = GetComponent<AudioSource>();
+        if (audio != null)
+        {
+            audio.Play();
+        }
+	}
+}
